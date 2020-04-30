7083821eb26074f5e2d5e44e26bbaf39499ada05
Ramela Ramnauth <ramelaramnauth2@gmail.com>
2020-04-30 19:55:52 -0400

    makde dotenv conditional

--- app/controller/subreddit.js
+++ app/controller/subreddit.js
@@ -1,5 +1,7 @@
 const Snoowrap = require('snoowrap');
-require('dotenv').config();
+if (process.env.NODE_ENV === 'development') {
+	require('dotenv').config();
+  }
 
 const client = new Snoowrap({
 	userAgent: 'subreddit-api',
