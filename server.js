1d16e794c58386d4ee03806911e83122fd34ab63
Ramela Ramnauth <ramelaramnauth2@gmail.com>
2020-04-30 23:09:29 -0400

    edited server.js

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
+    if ('OPTIONS' == req.method) {
+      res.sendStatus(200);
+    }
+    else {
+      next();
   });
 
 app.use('/api/v1', subredditRoutes);
