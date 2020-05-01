1232346a3ee57944665f33e866988d586abd6cc0
Ramela Ramnauth <55069265+ramelaaa@users.noreply.github.com>
2020-05-01 10:35:45 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,22 +4,22 @@ const app = express();
 const subredditRoutes = require('./app/routes/subreddit');
 const HttpError = require('./app/models/http-error');
 
-const cors = require('cors')
+// const cors = require('cors')
 
-app.use(cors())
+// app.use(cors())
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//     if ('OPTIONS' == req.method) {
-//       res.sendStatus(200);
-//     }
-//     else {
-//       next();
-//     }
-//   });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "localhost:3000");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    if ('OPTIONS' == req.method) {
+      res.sendStatus(200);
+    }
+    else {
+      next();
+    }
+  });
 
 app.use('/api/v1', subredditRoutes);
 
