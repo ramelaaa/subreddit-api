5267995830612733cec5b8b46eb749ea422d31fa
Ramela Ramnauth <ramelaramnauth2@gmail.com>
2020-04-30 23:05:55 -0400

    edited server.js

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const HttpError = require('./app/models/http-error');
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
   });
 
