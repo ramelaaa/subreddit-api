ec52b0c5b5e82aa79c517aba0e5933bf037d4cee
Ramela Ramnauth <55069265+ramelaaa@users.noreply.github.com>
2020-05-01 10:37:45 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,18 +8,31 @@ const HttpError = require('./app/models/http-error');
 
 // app.use(cors())
 
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "localhost:3000");
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//     if ('OPTIONS' == req.method) {
+//       res.sendStatus(200);
+//     }
+//     else {
+//       next();
+//     }
+//   });
+
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "localhost:3000");
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    if ('OPTIONS' == req.method) {
-      res.sendStatus(200);
-    }
-    else {
-      next();
-    }
-  });
+  res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Expose-Headers', 'Content-Length');
+  res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
+  if (req.method === 'OPTIONS') {
+    return res.send(200);
+  } else {
+    return next();
+  }
+});
 
 app.use('/api/v1', subredditRoutes);
 
